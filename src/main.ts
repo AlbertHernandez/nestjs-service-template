0710c63099da8ddb20c9b5db102b4285db3f0b02
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-04 11:06:31 +0100

    feat: migrate from express to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const configService = app.get(ConfigService);
   const port = configService.get<string>("PORT", "3000");
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
   const logger = app.get(Logger);
   logger.log(`App is ready and listening on port ${port} 🚀`);
--- tests/e2e/health.test.ts
+++ tests/e2e/health.test.ts
@@ -1,19 +1,29 @@
-import { INestApplication } from "@nestjs/common";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 import { Test, TestingModule } from "@nestjs/testing";
 import request from "supertest";
 
 import { AppModule } from "../../src/app.module";
 
 describe("HealthController (e2e)", () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
   it("/health (GET)", () => {
