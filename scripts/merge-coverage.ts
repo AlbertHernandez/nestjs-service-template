41df8b39d8c89fc4f1262ce2501386fadbd56002
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-04 12:02:28 +0100

    ci: use nyc instead of instanbul for merging coverage

---
+++ scripts/merge-coverage.ts
@@ -0,0 +1,19 @@
+import { execSync } from "child_process";
+import fs from "fs-extra";
+import path from "path";
+
+const REPORTS_PATH = path.resolve(process.cwd(), ".nyc_output");
+const COVERAGE_PATH = path.resolve(process.cwd(), "coverage");
+
+fs.emptyDirSync(REPORTS_PATH);
+fs.copyFileSync(
+  `${COVERAGE_PATH}/unit/coverage-final.json`,
+  `${REPORTS_PATH}/unit-coverage.json`,
+);
+fs.copyFileSync(
+  `${COVERAGE_PATH}/e2e/coverage-final.json`,
+  `${REPORTS_PATH}/e2e-coverage.json`,
+);
+execSync(`nyc report --reporter lcov --report-dir ${COVERAGE_PATH}/merge`, {
+  stdio: "inherit",
+});
