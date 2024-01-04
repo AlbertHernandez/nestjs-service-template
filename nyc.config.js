1dfecfb75dada4c9e73eaac072cbfa8719ca0ade
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-04 12:10:15 +0100

    ci: configure nyc config file for coverage

---
+++ nyc.config.js
@@ -0,0 +1,11 @@
+const MIN_COVERAGE = 80;
+
+const config = {
+  "check-coverage": false,
+  branches: MIN_COVERAGE,
+  lines: MIN_COVERAGE,
+  functions: MIN_COVERAGE,
+  statements: MIN_COVERAGE,
+};
+
+module.exports = config;
