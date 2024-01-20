41dc3380b7366a0c74b391db09267f29493e7043
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-20 10:13:07 +0100

    chore: just specify path to swcrc in the tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
-import fs from "fs-extra";
 import type { Config } from "jest";
-
-const swcConfig = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, "utf-8"));
 
 const config: Config = {
   transform: {
-    "^.+\\.(t|j)s$": ["@swc/jest", swcConfig],
+    "^.+\\.(t|j)s$": ["@swc/jest", { configFile: ".swcrc" }],
   },
   testEnvironment: "node",
   cacheDirectory: ".tmp/jestCache",
