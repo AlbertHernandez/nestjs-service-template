29d87d1bfa7b688df7efce7c82c108abf146cb8c
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-20 09:37:34 +0100

    chore: remove unnecessary comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,6 @@ import { LoggerModule } from "@core/logger/logger.module";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
-// hello
-
-// hello
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
