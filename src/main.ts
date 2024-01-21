163c53ec3e3cf1cab202290268560bbdb8336730
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-21 22:55:33 +0100

    style: use unicorn recommended

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ bootstrap().catch(handleError);
 function handleError(error: unknown) {
   // eslint-disable-next-line no-console
   console.error(error);
+  // eslint-disable-next-line unicorn/no-process-exit
   process.exit(1);
 }
 
