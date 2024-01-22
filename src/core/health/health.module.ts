9d35e16763784b53088eb46e55c4815ad41c83d4
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-22 12:40:49 +0100

    refactor: do not import logger module because is already global

--- src/core/health/health.module.ts
+++ src/core/health/health.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
 
-import { LoggerModule } from "@core/logger/logger.module";
-
 import { HealthController } from "./api/health.controller";
 
 @Module({
-  imports: [LoggerModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
