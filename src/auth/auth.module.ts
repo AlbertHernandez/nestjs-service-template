3704c94a0d01424a9ad8ea8970f34e0ce55366c0
elhadj-Komvoy <elhadj@komvoy.com>
2024-02-29 16:54:51 +0100

    feat: Implementación de JWT y passport-js-jwt Descripción:
    Se instalaron las dependencias jwt y passport-js-jwt para la autenticación mediante JWT.
    Se configuró la autenticación JWT en el módulo principal para estar disponible en toda la aplicación.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,27 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        // Dev-Nota: Tratar de acceder a las variables de entorno con el método get de la instancia de ConfigService.
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        };
+      },
+    }),
+  ],
   // Esto hace el servicio auth.service.ts disponible para otros módulos.
   exports: [TypeOrmModule]
 })
