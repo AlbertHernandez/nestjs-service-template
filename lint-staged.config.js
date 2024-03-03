a6aa8756124ed0c950140177121a5a0ad63cfa70
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-03-03 13:01:18 +0100

    test: add vitest lintstaged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   "**/*.ts?(x)": () => "tsc -p tsconfig.prod.json --noEmit",
-  "*.{js,jsx,ts,tsx}": ["npm run lint"],
+  "*.{js,jsx,ts,tsx}": ["npm run lint", "vitest related --run"],
   "*.{md,json}": "prettier --write",
 };
 
