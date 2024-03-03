ecf5046719d66cb6aa1ee4d95d4c82a0eac000cf
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-03-03 12:46:22 +0100

    test: add coverage flag in cli

--- create-vitest-test-config.ts
+++ create-vitest-test-config.ts
@@ -5,13 +5,13 @@ export const createVitestTestConfig = (
   testingType: "unit" | "e2e",
 ): InlineConfig => {
   return {
+    root: "./",
     globals: true,
     isolate: false,
     passWithNoTests: true,
     include: [`tests/${testingType}/**/*.test.ts`],
     env: loadEnv("test", process.cwd(), ""),
     coverage: {
-      enabled: true,
       provider: "istanbul",
       reporter: ["text", "json", "html"],
       reportsDirectory: `coverage/${testingType}`,
