bfec1c0fdc316af3d2b564a447385b08ec390724
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-26 08:35:07 +0200

    chore(typos): improve typos by ignoring files

--- scripts/check_typos.sh
+++ scripts/check_typos.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
-if ! command -v typos >/dev/null 2>&1; then
-    echo "Typos CLI tool is not installed, aborting typo check."
-    echo "If you want to install it, you can run 'brew install typos-cli'"
-    exit 0 # We don't want to fail the build if the tool is not installed
-fi
+check_typos_installed() {
+    if ! command -v typos >/dev/null 2>&1; then
+        echo "Typos CLI tool is not installed, aborting typo check."
+        echo "If you want to install it, you can run 'brew install typos-cli'"
+        exit 0 # We don't want to fail the build if the tool is not installed
+    fi
+}
 
-if [ "$#" -eq 0 ]; then
-    files="."
-else
-    files="$@"
-fi
+get_files() {
+    if [ "$#" -eq 0 ]; then
+        echo "."
+    else
+        echo "$@"
+    fi
+}
 
-typos $files
+filter_files() {
+    IGNORE_EXTENSIONS=("png" "snap" "jpg")
+
+    local files="$1"
+    local filtered=""
+    for file in $files; do
+        ignore_file=false
+        for ext in "${IGNORE_EXTENSIONS[@]}"; do
+            if [[ $file == *.$ext ]]; then
+                ignore_file=true
+                break
+            fi
+        done
+        if [ "$ignore_file" = false ]; then
+            filtered="$filtered $file"
+        fi
+    done
+    echo "$filtered"
+}
+
+convert_to_relative_paths() {
+    local files="$1"
+    local current_dir=$(pwd)
+    local relative=""
+    for file in $files; do
+        relative="$relative ${file#$current_dir/}"
+    done
+    echo "$relative"
+}
+
+check_typos_installed
+absolute_path_files=$(get_files "$@")
+filtered_files=$(filter_files "$absolute_path_files")
+relative_files=$(convert_to_relative_paths "$filtered_files")
+typos $relative_files
